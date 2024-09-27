8e8e857314d27461c35c79fc67bf21bf22b033c4
Isidor <isidor.bj@gmail.com>
2024-09-27 16:16:44 +0200

    fikser lister for alle verdier og konverterer til riktig datatype

--- fil1.py
+++ fil1.py
@@ -1,8 +1,9 @@
+import matplotlib.pyplot as plt
+
 with open("temperatur_trykk_met_samme_rune_time_datasett.csv.txt", "r", encoding="UTF8")as file:
     file.readline()
-    file.readline()[:-1]
     linjer = file.readlines()
-
+    linjer = linjer[:-2]
 
 navn_liste = []
 stasjon_liste = []
@@ -10,11 +11,20 @@ tid_liste = []
 lufttemp_liste = []
 lufttrykk_liste = []
 
+
+new_temp = []
 for linje in linjer:
-    navn_liste.append(linje.split(";")[0])
-    stasjon_liste.append(linje.split(";")[1])
-    tid_liste.append(linje.split(";")[2])
-    lufttemp_liste.append(linje.split(";")[3])
-    lufttrykk_liste.append(linje.split(";")[4])
+    navn_liste.append(linje.strip().split(";")[0])
+    stasjon_liste.append(linje.strip().split(";")[1])
+    tid_liste.append(linje.strip().split(";")[2])
+    lufttemp_liste.append(float(linje.replace(",",".").strip().split(";")[3]))
+    lufttrykk_liste.append(float(linje.replace(",",".").strip().split(";")[4]))
     
-print(tid_liste)
+
+print(lufttrykk_liste)
+
+
+plt.plot(tid_liste, lufttemp_liste)
+plt.legend()
+plt.show()
+
--- fil2.py
+++ fil2.py
@@ -1,20 +1,23 @@
+import matplotlib.pyplot as plt
+
 with open("trykk_og_temperaturlogg_rune_time.csv.txt", "r", encoding="UTF8")as file:
     file.readline()
     file.readline()[:-1]
     linjer = file.readlines()
 
 
-navn_liste = []
-stasjon_liste = []
 tid_liste = []
-lufttemp_liste = []
-lufttrykk_liste = []
+tid_siden_start_liste = []
+trykk_liste = []
+abs_trykk_liste = []
+temp_liste = []
 
 for linje in linjer:
-    navn_liste.append(linje.split(";")[0])
-    stasjon_liste.append(linje.split(";")[1])
-    tid_liste.append(linje.split(";")[2])
-    lufttemp_liste.append(linje.split(";")[3])
-    lufttrykk_liste.append(linje.split(";")[4])
+    tid_liste.append(linje.split(";")[0])
+    tid_siden_start_liste.append(linje.split(";")[1])
+    trykk_liste.append(linje.split(";")[2])
+    abs_trykk_liste.append(linje.split(";")[3])
+    temp_liste.append(linje.split(";")[4])
     
 print(tid_liste)
+
