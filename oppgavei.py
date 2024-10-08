98c06d72f2bf8c33061a65f0119380221a05052b
Isidor <isidor.bj@gmail.com>
2024-10-08 15:35:26 +0200

    oppgave i

--- oppgavei.py
+++ oppgavei.py
@@ -1,21 +1,16 @@
 import matplotlib.pyplot as plt
 import datetime
 with open("trykk_og_temperaturlogg_rune_time.csv.txt", "r", encoding="UTF8")as file:
-    file.readline()
     file.readline()[:-1]
     linjer = file.readlines()
     
-
 tid_liste = []
 tid_siden_start_liste = []
 trykk_liste = []
 abs_trykk_liste = []
 temp_liste = []
-ampm_6 = []
-tidplot_6 = []
 
 for linje in linjer:
-    tid_liste.append(linje.strip().split(";")[0])
     tid_siden_start_liste.append(linje.split(";")[1])
     trykk_liste.append(linje.replace(",", ".").strip().split(";")[2])
     abs_trykk_liste.append(float(linje.replace(",", ".").strip().split(";")[3]))
@@ -24,13 +19,19 @@ for linje in linjer:
 plot_tid1 = []
 pm_og_am = []
 plot_tidfinal = []
+trykkbar_liste = []
 
+
+for linje in linjer:
+    if linje.split(";")[2] != "":
+        trykkbar_liste.append(float(linje.replace(",", ".").split(";")[2]))
+        tid_liste.append(linje.split(";")[0]) 
 
 for x in tid_liste:
-    if x[-2:] == "pm" and x[11:12] != "12":
+    if x[-2:] == "pm":
         pm_og_am.append(x)
 
-    elif x[-2:] == "am" or x[-2:] == "pm":
+    elif x[-2:] == "am":
         pm_og_am.append(x)
 
     elif x[-2:] != "am" and x[-2:] != "pm":
@@ -38,19 +39,17 @@ for x in tid_liste:
     
 
 
-for x in range(0, len(plot_tid1), 6):
-    plot_tidfinal.append(datetime.datetime.strptime(plot_tid1[x], "%m.%d.%Y %H:%M"))
+
+for x in plot_tid1:
+    plot_tidfinal.append(datetime.datetime.strptime(x, "%m.%d.%Y %H:%M"))
 
 for x in pm_og_am:
     if x[17:22] == "08 pm":
-        ampm_6.append(datetime.datetime.strptime(x, "%m/%d/%Y %I:%M:%S %p"))
+        plot_tidfinal.append(datetime.datetime.strptime(x, "%m/%d/%Y %I:%M:%S %p"))
 
     elif x[17:22] == "08 am":
-        ampm_6.append(datetime.datetime.strptime(x[0:19], "%m/%d/%Y %H:%M:%S"))
+        plot_tidfinal.append(datetime.datetime.strptime(x[0:19], "%m/%d/%Y %H:%M:%S"))
 
 
-trykkbar_liste = []
-for x in trykk_liste:
-    if x != "":
-        trykkbar_liste.append(x)
-
+plt.plot(plot_tidfinal, trykkbar_liste)
+plt.show()
